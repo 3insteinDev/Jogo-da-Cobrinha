884dae195c39b82787cd730cc279dc886dd920c8
eins10Week <einstein.hellmeister@gmail.com>
2021-04-03 01:31:16 -0300

    definindo movimentos da cobrinha

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "rigth";
 
 function criarBG() {
     context.fillStyle = "Black";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
-
 
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
@@ -20,5 +20,26 @@ function criarCobrinha(){
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+   criarBG();
+   criarCobrinha();
+   
+   let snakeX = snake[0].x; 
+   let snakeY = snake[0].y;
+
+   if(direction == "rigth") snakeX += box;
+   if(direction == "left") snakeX -= box;
+   if(direction == "up") snakeY -= box;
+   if(direction == "down") snakeY += box;
+
+   snake.pop();
+
+   let newHead = {
+       x: snakeX,
+       y: snakeY,
+   }
+
+   snake.unshift(newHead)
+}
+
+let jogo = setInterval(iniciarJogo, 100);
